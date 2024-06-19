b43ec1ef80abd8a320f26d79d4057ff6104be858
MatthewCourt <matthewcourt0@gmail.com>
2024-06-19 04:48:37 +0000

    updating changes

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,39 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+
+SELECT
+  type,
+  name,
+  price,
+  description
+FROM
+  Dishes
+ORDER BY
+  price
+
+SELECT
+  type,
+  name,
+  price,
+  description
+FROM
+  Dishes
+where
+  type = 'Appetizer'
+  or
+  type = 'Beverage'
+ORDER BY
+  type
+
+SELECT
+  type,
+  name,
+  price,
+  description
+FROM
+  Dishes
+where
+  type != 'Beverage'
+ORDER BY
+  type
