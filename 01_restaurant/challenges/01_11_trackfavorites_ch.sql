532098e32766591315d210fcd8ea30b3101659a7
MatthewCourt <matthewcourt0@gmail.com>
2024-06-20 03:56:47 +0000

    update changes

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -3,4 +3,13 @@
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
 
-SELECT
+SELECT * from customers where lastname = 'Goldwater'
+
+SELECT * from dishes where name = 'Quinoa Salmon Salad'
+
+Update
+  customers
+Set
+  favoritedish = (SELECT dishid from dishes where name = 'Quinoa Salmon Salad')
+where
+  customerid = (SELECT customerid from customers where lastname = 'Goldwater')
--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,22 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+-- firstname, lastname, email
+
+SELECT
+  c.customerid,
+  c.firstname,
+  c.lastname,
+  c.email,
+  count(o.orderid) as amount
+from
+  orders o
+join
+  customers c
+on
+  c.customerid = o.customerid
+group by
+  o.customerid
+order by
+  amount desc
+limit 10
